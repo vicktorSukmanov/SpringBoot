d6c1d3af1fd61721db439de7b6c0a5c6764f844d
Viktor Sukmanov <vicktor.sukmanov@yandex.ru>
2023-11-15 22:32:55 +0300

    Правки

--- src/main/java/ru/usersshow/SpringBoot/dao/UserDao.java
+++ src/main/java/ru/usersshow/SpringBoot/dao/UserDao.java
@@ -7,9 +7,13 @@ import java.util.List;
 
 public interface UserDao {
     List<User> getListUser();
+
     void createUser(User user);
+
     void updateUser(User user);
+
     User readUser(long id);
+
     void deleteUser(long id);
 
 }
--- src/main/java/ru/usersshow/SpringBoot/service/UserService.java
+++ src/main/java/ru/usersshow/SpringBoot/service/UserService.java
@@ -6,10 +6,15 @@ import ru.usersshow.SpringBoot.model.User;
 import java.util.List;
 
 public interface UserService {
+
     List<User> getListUser();
+
     void createUser(User user);
+
     void updateUser(User user);
+
     User readUser(long id);
+
     void deleteUser(long id);
 
 }
